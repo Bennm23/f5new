54a8fbf6c30c2f95bd1a39d3d98fd681b986ef44
reyesjl <reyesjl.nine@hotmail.com>
2023-11-20 02:25:36 +0000

    User form frontend; Templates and styles;

--- project/f5index/urls.py
+++ project/f5index/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-
 app_name='index'
 urlpatterns = [
     path('', views.index, name='home'),
+    path('signup/', views.create_member, name='create_member'),
 ]
 
--- project/f5index/views.py
+++ project/f5index/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .models import Member
 
 # Create your views here.
 
@@ -7,4 +9,15 @@ def index(request):
     context = {}
     return render(request, "f5index/index.html", context)
 
-
+def create_member(request):  
+    if request.POST == 'POST':  
+        form = UserCreationForm()  
+        if form.is_valid():  
+            form.save()  
+        messages.success(request, 'Account created successfully')  
+    else:  
+        form = UserCreationForm()  
+    context = {  
+        'form':form  
+    }  
+    return render(request, 'f5index/create_member.html', context)
