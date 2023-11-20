3dc9724caf80a40bd289856a26a9e3d0cf2e2ef8
Benn Mellinger <bennm23@gmail.com>
2023-11-19 22:27:51 -0500

    Register blogs model and include ckeditor

--- project/f5blogs/models.py
+++ project/f5blogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from ckeditor.fields import RichTextField
+
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    create_date = models.DateTimeField("date created", null=True)
+    content = RichTextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
--- project/firstfive/settings.py
+++ project/firstfive/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'f5index',
     'f5store',
+    'f5blogs',
 
 ]
 
