486febe35bdec72befc287da2c09d6efdf71ec2d
reyesjl <reyesjl.nine@hotmail.com>
2023-11-20 23:44:07 +0000

    Added login and signup forms;

--- project/f5index/urls.py
+++ project/f5index/urls.py
@@ -6,5 +6,6 @@ app_name='index'
 urlpatterns = [
     path('', views.index, name='home'),
     path('signup/', views.create_member, name='create_member'),
+    path('login/', views.login_member, name='login_member'),
 ]
 
--- project/f5index/views.py
+++ project/f5index/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from .models import Member
-
-# Create your views here.
-
+from django.shortcuts import redirect, render
+from .forms import CreateUserForm, LoginUserForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
     context = {}
@@ -11,12 +8,28 @@ def index(request):
 
 def create_member(request):  
     if request.POST == 'POST':  
-        form = UserCreationForm()  
+        form = CreateUserForm()  
         if form.is_valid():  
             form.save()  
     else:  
-        form = UserCreationForm()  
+        form = CreateUserForm()  
     context = {  
         'form':form  
     }  
     return render(request, 'f5index/create_member.html', context)
+
+def login_member(request):  
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            # Authenticate the user
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                # Log the user in
+                login(request, user)
+                return redirect('blog:index')  # Redirect to a success page or another URL
+    else:
+        form = LoginUserForm()  
+
+    context = {'form': form}
+    return render(request, 'f5index/login_member.html', context)
