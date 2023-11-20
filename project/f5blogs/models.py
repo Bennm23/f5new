567165b32c3c843553ca0f4811cdc37cacf9321b
reyesjl <reyesjl.nine@hotmail.com>
2023-11-20 19:44:54 +0000

    Added form styles; Added tag to blog posts;

--- project/f5blogs/models.py
+++ project/f5blogs/models.py
@@ -4,7 +4,17 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 class BlogPost(models.Model):
+    TAG_CHOICES = [
+        ('mental', 'Mental'),
+        ('physical', 'Physical'),
+        ('community', 'Community'),
+        ('teams', 'Teams'),
+        ('players', 'Players'),
+        ('coaches', 'Coaches'),
+        # Add more choices as needed
+    ]
     title = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200, choices=TAG_CHOICES)
     create_date = models.DateTimeField('date created', auto_now_add=True)
     content = RichTextField(blank=True, null=True)
 
