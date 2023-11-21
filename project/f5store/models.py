38a58254c4661bc618ced0c10d6a50346cb836a3
reyesjl <reyesjl.nine@hotmail.com>
2023-11-21 06:13:43 +0000

    added product model

--- project/f5store/models.py
+++ project/f5store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+  CATEGORY_CHOICES = [
+    ('apparel', 'Apparel'),
+    ('caps', 'Caps'),
+    ('teams', 'Teams'),
+    ('players', 'Players'),
+    ('short', 'Short sleeve'),
+    ('long', 'Long sleeve'),
+    ('shoes', 'Shoes'),
+    ('accessories', 'Accessories'),
+  ]
+
+  MATERIAL_CHOICES = [
+    ('cotton', 'Cotton'),
+    ('leather', 'Leather'),
+    ('polyester', 'Polyester'),
+  ]
+  
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  categories = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
+  materials = models.CharField(max_length=255, choices=MATERIAL_CHOICES)
+
+  def __str__(self):
+      return self.name
