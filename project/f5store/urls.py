9ded904eeb9e5397d0db5468482aef0e449b6082
reyesjl <reyesjl.nine@hotmail.com>
2023-11-21 06:42:29 +0000

    added urls; added views;

--- project/f5store/urls.py
+++ project/f5store/urls.py
@@ -6,4 +6,9 @@ from . import views
 app_name='store'
 urlpatterns = [
     path('', views.index, name='home'),
+    path('create/', views.create, name='create'),
+    path('<int:product_id>/detail', views.detail, name='detail'),
+    path('<int:product_id>/edit', views.edit, name='edit'),
+    path('<int:product_id>/delete', views.edit, name='delete'),
+
 ]
--- project/f5store/views.py
+++ project/f5store/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-# Create your views here.
+from .forms import ProductForm
+from .models import Product
 
 def index(request):
-    context = {}
-    return render(request, "f5store/index.html", context)
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'f5store/product_list.html', context)
+
+def create(request):
+    form = None
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('store:product_list'))
+    else:
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'f5store/product_entry.html', context)
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'f5store/detail_product.html', context)
+
+def edit(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+
+        if form.is_valid():
+            product = form.save()
+            return HttpResponseRedirect(reverse('store:product_detail', args=(product.id,)))
+    else:
+        form = ProductForm(instance=product)
+        context = {
+            'form': form,
+            'product_id': product_id,
+        }
+        return render(request, 'f5store/edit_product.html', context)
+
+def delete(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    return HttpResponseRedirect(reverse('store:product_list'))
