50d716e62f25138b6d36ebd1316648c017647498
reyesjl <reyesjl.nine@hotmail.com>
2023-11-21 16:05:05 +0000

    Added Tag class for blogs and many to many relation;

--- project/f5blogs/models.py
+++ project/f5blogs/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
-# Create your models here.
-class BlogPost(models.Model):
+class Tag(models.Model):
     TAG_CHOICES = [
         ('none', 'None/Unspecified'),
         ('mental', 'Mental'),
@@ -13,8 +12,15 @@ class BlogPost(models.Model):
         ('coaches', 'Coaches'),
         # Add more choices as needed
     ]
+
+    name = models.CharField(max_length=50, choices=TAG_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    tags = models.CharField(max_length=200, choices=TAG_CHOICES, default='none')
+    tags = models.ManyToManyField(Tag, blank=True)
     create_date = models.DateTimeField('date created', auto_now_add=True)
     content = RichTextField(blank=True, null=True)
 
