b92cff21259f275b3c722557862f284e3180458a
jose <reyesjl.nine@hotmail.com>
2023-11-22 21:22:07 +0000

    added TeamSearchForm to views and Forms file;

--- project/f5teams/forms.py
+++ project/f5teams/forms.py
@@ -2,6 +2,10 @@ from .models import Team
 
 from django.forms import ModelForm, widgets
 
+class TeamSearchForm(forms.Form):
+    search_query = forms.CharField(label='Search', required=False)
+    
+
 class TeamForm(ModelForm):
     class Meta:
         model = Team
--- project/f5teams/views.py
+++ project/f5teams/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .forms import TeamForm
+from .forms import TeamForm, TeamSearchForm
 from .models import Team
 from f5index.models import Member
 
