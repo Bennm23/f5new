4b19569403dccf4ea2aa1c9b3ea9733e311786cc
reyesjl <reyesjl.nine@hotmail.com>
2023-11-23 20:34:12 +0000

    product search nearly complete

--- project/f5store/views.py
+++ project/f5store/views.py
@@ -2,12 +2,47 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .forms import ProductForm
+from .forms import ProductForm, ProductSearchForm
 from .models import Product
 
 def index(request):
+    if request.method == 'GET':
+        form = ProductSearchForm(request.GET)
+        if form.is_valid():
+            # Process the form data and filter products accordingly
+            products = Product.objects.all()
+
+            # Filter by search query
+            search_query = form.cleaned_data.get('search_query')
+            if search_query:
+                products = products.filter(name__icontains=search_query)
+
+            # Filter by category
+            category = form.cleaned_data.get('categories')
+            if category:
+                products = products.filter(categories=categories)
+
+            # Filter by material
+            material = form.cleaned_data.get('materials')
+            if material:
+                products = products.filter(materials=materials)
+
+            # Filter by price range
+            min_price = form.cleaned_data.get('min_price')
+            max_price = form.cleaned_data.get('max_price')
+            if min_price is not None:
+                products = products.filter(price__gte=min_price)
+            if max_price is not None:
+                products = products.filter(price__lte=max_price)
+
+    else:
+        # If it's not a GET request, initialize the form
+        form = ProductSearchForm()
+        products = Product.objects.all()
+
     context = {
-        'products': Product.objects.all()
+        'search_form': form,
+        'products': products
     }
     return render(request, 'f5store/products_home.html', context)
 
