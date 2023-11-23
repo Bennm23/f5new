eeb71bdb6f96b84e5d68e7509f9a938079a609d9
reyesjl <reyesjl.nine@hotmail.com>
2023-11-23 19:10:43 +0000

    added ProductSearchForm; added constants;

---
+++ project/f5store/constants.py
@@ -0,0 +1,17 @@
+CATEGORY_CHOICES = [
+    ('apparel', 'Apparel'),
+    ('caps', 'Caps'),
+    ('tickets', 'Tickets'),
+    ('teams', 'Teams'),
+    ('players', 'Players'),
+    ('short', 'Short sleeve'),
+    ('long', 'Long sleeve'),
+    ('shoes', 'Shoes'),
+    ('accessories', 'Accessories'),
+]
+
+MATERIAL_CHOICES = [
+    ('cotton', 'Cotton'),
+    ('leather', 'Leather'),
+    ('polyester', 'Polyester'),
+]
--- project/f5store/forms.py
+++ project/f5store/forms.py
@@ -1,6 +1,16 @@
-from .models import Product
+# f5store/forms.py 
 
+from django import forms
+from .models import Product
 from django.forms import ModelForm, widgets
+from .constants import CATEGORY_CHOICES, MATERIAL_CHOICES
+
+class ProductSearchForm(forms.Form):
+    search_query = forms.CharField(label='Search', required=False)
+    category = forms.ChoiceField(choices=Product.CATEGORY_CHOICES, required=False)
+    material = forms.ChoiceField(choices=Product.MATERIAL_CHOICES, required=False)
+    min_price = forms.DecimalField(label='Min Price', required=False)
+    max_price = forms.DecimalField(label='Max Price', required=False)
 
 class ProductForm(ModelForm):
 	class Meta:
