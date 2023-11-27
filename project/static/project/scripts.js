6fdc843a8875e8c708b89f1868776551254b4894
reyesjl <reyesjl.nine@hotmail.com>
2023-11-27 15:36:49 +0000

    search form toggle works now

--- project/static/project/scripts.js
+++ project/static/project/scripts.js
@@ -1,1 +1,10 @@
-console.log("test");
+
+// Toggle visibility of search forms on searchables.
+function toggleSearchForm() {
+  var searchFormContainer = document.getElementById("searchFormContainer");
+  if (searchFormContainer.style.display === "none" || searchFormContainer.style.display === "") {
+    searchFormContainer.style.display = "block";
+  } else {
+    searchFormContainer.style.display = "none";
+  }
+}
