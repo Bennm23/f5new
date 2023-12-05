b9421cd12279e962f2e07c3282f7a5dd19a79ab5
reyesjl <reyesjl.nine@hotmail.com>
2023-12-05 03:35:11 -0500

    profile and errors;

--- project/f5members/urls.py
+++ project/f5members/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('editMember/', views.edit_member, name='edit_member'),
     path('verifySent/', views.verify_sent, name="verify_sent"),
     path('verify/<str:verification_code>/', views.verify_user, name='verify_user'),
-    path('profile/<str:member_username>/', views.public_profile, name='public_profile')
+    path('profile/<str:member_username>/', views.public_profile, name='public_profile'),
+    path('error/', views.error, name='error')
 ]
