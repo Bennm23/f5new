7a42903b09bdecb8ec1cdb1448517e0775cc883c
reyesjl <reyesjl.nine@hotmail.com>
2023-12-05 03:34:19 -0500

    added types, not visisble to users;

--- project/f5members/constants.py
+++ project/f5members/constants.py
@@ -1,5 +1,14 @@
 USER_TYPES = [
   ('player', 'Player'),
   ('coach', 'Coach'),
-  ('fan_other', 'Fan/Other'),
+  ('fan', 'Fan'),
+]
+
+USER_TEST_TYPES = [
+  ('admin', 'Admin'),
+  ('editor', 'Editor'),
+  ('support', 'Support'),
+  ('player', 'Player'),
+  ('coach', 'Coach'),
+  ('fan', 'Fan'),
 ]
