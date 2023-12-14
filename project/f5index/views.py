4d170a878eacf3ce6802556afbfc4826aee12b49
reyesjl <reyesjl.nine@hotmail.com>
2023-12-14 18:00:45 -0500

    home page blog displays;

--- project/f5index/views.py
+++ project/f5index/views.py
@@ -1,30 +1,17 @@
+import random
 import feedparser
 from f5teams.models import Team
 from django.contrib import messages
 from f5members.models import Member
 from f5blogs.models import BlogPost
-from django.core.cache import cache
 from .forms import SupportSubmissionForm
 from django.shortcuts import get_object_or_404, redirect, render
 
 def index(request):
-    # Attempt rw_feed fetch from cache
-    rw_feed = cache.get('rw_feed')
-
-    if not rw_feed:
-        # fetch rw_feed
-        rw_url = 'https://www.rugbyworld.com/feed'
-        rw_feed = feedparser.parse(rw_url)
-
-        # cache for 6 hours
-        cache.set('rw_feed', rw_feed, 21600)
-
-    # Extract the first post
-    feature_post, *remaining_posts = rw_feed.entries
+    blogs = BlogPost.objects.all()
 
     context = {
-        'feature_post': feature_post,
-        'blogs': remaining_posts,
+        'blogs': blogs,
     }
     
     return render(request, "f5index/index.html", context)
