6b8b0fc464a960dbab7780d0a8b792543db3987b
reyesjl <reyesjl.nine@hotmail.com>
2023-12-15 20:42:04 -0500

    forms;

--- project/f5blogs/forms.py
+++ project/f5blogs/forms.py
@@ -2,22 +2,25 @@ from django import forms
 from .models import BlogPost, Tag
 from .constants import TAG_CHOICES
 from django.forms import CheckboxSelectMultiple, ModelForm
+from ckeditor.widgets import CKEditorWidget
 
 class BlogSearchForm(forms.Form):
     search_query = forms.CharField(label='Search', required=False)
     tags = forms.ChoiceField(choices=TAG_CHOICES, required=False)
     
 class BlogForm(ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), 
+                                              required=False,
+                                              widget=CheckboxSelectMultiple, 
+                                              blank=True)
     class Meta:
         model = BlogPost
         fields = ['title', 'tags', 'content']
         widgets = {
             'tags': CheckboxSelectMultiple(),
+            'content': CKEditorWidget(),
         }
-        tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), 
-                                              required=False,
-                                              widget=CheckboxSelectMultiple, 
-                                              blank=True)
     
     def clean_content(self):
         data : str = self.cleaned_data['content']
