3ca8127c594063d87d8b2e940c1d8df5fbac51ee
reyesjl <reyesjl.nine@hotmail.com>
2023-12-21 20:50:06 -0500

    detail route;l

--- project/f5store/urls.py
+++ project/f5store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('create/', views.create, name='create_product'),
     path('stripe/products/', views.get_stripe_products, name='stripe_products'),
-    path('product/<str:product_id>/detail', views.get_stripe_product_detail, name='product_detail'),
+    path('product/<str:product_id>/detail', views.detail, name='product_detail'),
     path('<int:product_id>/edit', views.edit, name='product_edit'),
     path('<int:product_id>/delete', views.edit, name='product_delete'),
 ]
