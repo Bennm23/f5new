55b7ed82b5ff02c99aceb455c1d7515f216638d3
reyesjl <reyesjl.nine@hotmail.com>
2024-01-02 10:17:40 -0500

    add initiate checkout; add webhook for completed event;

--- project/f5store/views.py
+++ project/f5store/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+import os
+from urllib.parse import quote
+from django.conf import settings
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+import stripe
 
 from .forms import ProductForm, ProductSearchForm
 from .models import Product, Category, Material
 
-from .services import create_stripe_product, create_stripe_product_with_price
+from .services import create_stripe_product, create_stripe_product_with_price, create_stripe_checkout_session
 
 def index(request):
     # Fetch the most recent three products for the 'recent_products' context
@@ -103,6 +108,67 @@ def delete(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     return HttpResponseRedirect(reverse('store:home'))
+
+def initiate_checkout(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    # Check if enough quantity is available
+    if product.quantity < 1:
+        # Redirect to an error page with a message
+        message = f'Not enough quantity available for {product.name}'
+        # URL-encode the message
+        encoded_message = quote(message)
+        
+        # Use Django's reverse function to construct the URL
+        error_url = reverse('store:checkout_error') + f'?message={encoded_message}'
+        return redirect(error_url)
+
+    session_url = create_stripe_checkout_session(product.stripe_price_id)
+
+    if session_url:
+        # Redirect to Stripe checkout
+        return redirect(session_url)
+    else:
+        message = 'Normal: This product is not a stripe product. It cannot be purchased.'
+        # URL-encode the message
+        encoded_message = quote(message)
+        
+        # Use Django's reverse function to construct the URL
+        error_url = reverse('store:checkout_error') + f'?message={encoded_message}'
+        return redirect(error_url)
+    
+@csrf_exempt
+def stripe_checkout_completed_webhook(request):
+    STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET_LOCAL')
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return JsonResponse({'status': 'invalid payload'}, status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return JsonResponse({'status': 'invalid signature'}, status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        
+        # Retrieve the product using Stripe price ID
+        price_id = session.line_items.data[0].price.id
+        product = get_object_or_404(Product, stripe_price_id=price_id)
+
+        # Decrement product quantity
+        if product.quantity > 0:
+            product.quantity -= 1
+            product.save()
+
+    return JsonResponse({'status': 'success'}, status=200)
 
 def checkout_error(request):
     message = request.GET.get('message', 'An error occured during checkout the checkout process, you will not be charged.')
