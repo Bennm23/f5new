59a7e3c9232438c763ed781a6e26f81eccd2d757
reyesjl <reyesjl.nine@hotmail.com>
2024-01-02 08:43:01 -0500

    add checkout error url; add checkout success url;

--- project/f5store/urls.py
+++ project/f5store/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('product/<str:product_id>/detail', views.detail, name='product_detail'),
     path('<int:product_id>/edit', views.edit, name='product_edit'),
     path('<int:product_id>/delete', views.edit, name='product_delete'),
+    path('checkout', views.initiate_checkout, name="checkout"),
+    path('checkout_error/', views.checkout_error, name="checkout_error"),
+    path('checkout_success/', views.checkout_success, name="checkout_success"),
 ]
